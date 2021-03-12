8f703312bd70bd28bd686ca5e23d6da3c7b37a34
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-03-12 14:31:16 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+"use strict";
+(function () {
+    // App root where i render all the html
+    const app = document.getElementById('app');
+    // All the templates
+    const formTemp = document.getElementById('project-input');
+    const projectTemp = document.getElementById('single-project');
+    const listTemp = document.getElementById('project-list');
+    // helpers
+    const cloneContent = (element) => document.importNode(element.content, true);
+    const appendNode = (node, element = app) => element.appendChild(node);
+    // Clone all the templates and append them
+    const formNode = cloneContent(formTemp);
+    appendNode(formNode);
+    const activeListNode = cloneContent(listTemp);
+    const finishedListNode = cloneContent(listTemp);
+    appendNode(activeListNode);
+    appendNode(finishedListNode);
+    //Add functionality to form
+    // changing the second list to be finished list.
+    const addListTitle = (element, title) => {
+        var _a;
+        const titleElement = (_a = element.querySelector('header')) === null || _a === void 0 ? void 0 : _a.querySelector('h2');
+        titleElement.innerText = title;
+    };
+    const activeList = app.querySelector('.projects');
+    const finishedList = app.querySelectorAll('.projects')[1];
+    // Adding id for the styling
+    finishedList.id = 'finished-projects';
+    // Adding titles to section headers
+    addListTitle(activeList, 'Active Projects');
+    addListTitle(finishedList, 'Finished Projects');
+    // Handling change
+    let titleInput, descInput, peopleInput;
+    const handleChange = (e) => {
+        const type = e.target.name;
+        if (e.target) {
+            const { value } = e.target;
+            switch (type) {
+                case 'title':
+                    titleInput = value;
+                    break;
+                case 'description':
+                    descInput = value;
+                    break;
+                case 'people':
+                    peopleInput = value;
+                    break;
+                default:
+                    throw new Error('Input Error');
+            }
+        }
+    };
+    const onChange = (element) => element.addEventListener('change', handleChange);
+    const title = app.querySelector('#title');
+    onChange(title);
+    const description = app.querySelector('#description');
+    onChange(description);
+    const people = app.querySelector('#people');
+    onChange(people);
+    // Handling Submit
+    const form = app.querySelector('form');
+    let handleDrag = e => {
+        console.log(e);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //Creating an element
+        const projectDiv = document.createElement('div');
+        // Creating the project html
+        const singleProject = `
+        <h4>Title: ${titleInput}</h4>
+        <p>Description: ${descInput}</p>
+        <span>Num. of Contributors: ${peopleInput}</span>
+    `;
+        // Appending the project html to the element
+        projectDiv.innerHTML = singleProject;
+        const projectNode = cloneContent(projectTemp);
+        const activeListUL = activeList.querySelector('ul');
+        const li = projectNode.childNodes[1];
+        li.appendChild(projectDiv);
+        li.addEventListener('drag', handleDrag);
+        appendNode(projectNode, activeListUL);
+        form.reset();
+    };
+    form.addEventListener('submit', handleSubmit);
+})();
---
+++ app.ts
@@ -0,0 +1,85 @@
+(function() {
+// App root where i render all the html
+const app = document.getElementById('app')! as HTMLDivElement;
+// All the templates
+const formTemp = document.getElementById('project-input')! as HTMLTemplateElement;
+const projectTemp = document.getElementById('single-project')! as HTMLTemplateElement;
+const listTemp = document.getElementById('project-list')! as HTMLTemplateElement;
+// helpers
+const cloneContent = (element: HTMLTemplateElement) => document.importNode(element.content, true)
+const appendNode = (node: DocumentFragment, element: HTMLElement=app) => element.appendChild(node) 
+// Clone all the templates and append them
+const formNode = cloneContent(formTemp);
+appendNode(formNode);
+const activeListNode = cloneContent(listTemp);
+const finishedListNode = cloneContent(listTemp);
+appendNode(activeListNode);
+appendNode(finishedListNode);
+//Add functionality to form
+// changing the second list to be finished list.
+const addListTitle = (element: HTMLTableSectionElement, title: string) => {
+    const titleElement: HTMLElement = element.querySelector('header')?.querySelector('h2')!;
+    titleElement.innerText = title
+}
+const activeList = app.querySelector('.projects')! as HTMLTableSectionElement;
+const finishedList = app.querySelectorAll('.projects')[1]! as HTMLTableSectionElement;
+// Adding id for the styling
+finishedList.id = 'finished-projects'
+// Adding titles to section headers
+addListTitle(activeList, 'Active Projects');
+addListTitle(finishedList, 'Finished Projects');
+// Handling change
+let titleInput:string, descInput: string, peopleInput: number;
+const handleChange = (e: any): void => {
+    const type: string = e.target.name 
+    if(e.target) {
+        const { value } = e.target
+        switch(type) {
+            case 'title':
+                titleInput = value;
+                break;
+            case 'description':
+                descInput = value;
+                break;
+            case 'people':
+                peopleInput = value;
+                break;
+            default:
+                throw new Error('Input Error')
+        }
+    }
+}
+const onChange = (element: HTMLInputElement): void => element.addEventListener('change', handleChange) 
+const title = app.querySelector('#title')! as HTMLInputElement
+onChange(title)
+const description = app.querySelector('#description')! as HTMLInputElement
+onChange(description)
+const people = app.querySelector('#people')! as HTMLInputElement
+onChange(people)
+// Handling Submit
+const form = app.querySelector('form')! as HTMLFormElement;
+let handleDrag: (e: Event) => void = e => {
+    console.log(e)
+}
+const handleSubmit = (e: Event): void => {
+    e.preventDefault();
+    //Creating an element
+    const projectDiv = document.createElement('div');
+    // Creating the project html
+    const singleProject: string = `
+        <h4>Title: ${titleInput}</h4>
+        <p>Description: ${descInput}</p>
+        <span>Num. of Contributors: ${peopleInput}</span>
+    `
+    // Appending the project html to the element
+    projectDiv.innerHTML = singleProject;
+    const projectNode = cloneContent(projectTemp);
+    const activeListUL = activeList.querySelector('ul')! as HTMLUListElement;
+    const li = projectNode.childNodes[1] as HTMLLIElement;
+    li.appendChild(projectDiv)
+    li.addEventListener('drag', handleDrag)
+    appendNode(projectNode, activeListUL)
+    form.reset()
+}
+form.addEventListener('submit', handleSubmit)
+})()
